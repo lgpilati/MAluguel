c218f95e3983e637a33f2d1c94401b1f53285cf7
Lucas <lucaspilati18@hotmail.com>
2020-03-31 12:40:53 -0300

    Primeiro Commit

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const config= {
+    apiKey: "AIzaSyBfr1BfBPDrcXGC5HC9hoeqM49UX1jGpUM",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderID: ""
+};
